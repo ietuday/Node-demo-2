d1a03c9fa944b4312f413ba7bfd67e0060f2d795
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-03-27 10:42:05 +0530

    Added session demo

--- Web_Development_with_Node_Express/meadowlark.js
+++ Web_Development_with_Node_Express/meadowlark.js
@@ -18,24 +18,24 @@ app.set('port', process.env.PORT || 3000);
 //     res.send('500 - Server Error');
 // });
 
-app.get('/', function(req, res) {
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
-});
+// app.get('/', function(req, res) {
+//     res.type('text/plain');
+//     res.send('Meadowlark Travel');
+// });
 
-app.get('/about', function(req, res) {
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
-});
+// app.get('/about', function(req, res) {
+//     res.type('text/plain');
+//     res.send('About Meadowlark Travel');
+// });
 
-// custom 404 page
-app.use(function(req, res, next) {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
-});
+// // custom 404 page
+// app.use(function(req, res, next) {
+//     res.type('text/plain');
+//     res.status(404);
+//     res.send('404 - Not Found');
+// });
 
-app.listen(app.get('port'), function() {
-    console.log('Express started on http://localhost:' +
-        app.get('port') + '; press Ctrl-C to terminate.');
-});
+// app.listen(app.get('port'), function() {
+//     console.log('Express started on http://localhost:' +
+//         app.get('port') + '; press Ctrl-C to terminate.');
+// });
