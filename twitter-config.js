a206d658ce3aab37eeb75993a02dd801deb2f12b
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-03-29 12:27:54 +0530

    Added books for sql

---
+++ twitter-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: '91oF5rcDGFZf308PBEk88w5aT',
+  consumer_secret: '4ZETUrONylx6QoS9BVkyKNCM8Uxu3qvbIAI63nqHx8d3YBXbV0',
+  access_token_key: '154776442-3n9NXarM0VjEXEsTW5DA8gCDDr0hSm2PqXo6qS4l',
+  access_token_secret: 'udB6nztY2jazlo13mmJLczq0x780frVqhBjwOvTLju1nu'
+}
